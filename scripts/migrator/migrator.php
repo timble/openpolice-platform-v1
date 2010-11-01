6840dd25ed9004d90e3eacccbcb185d7ba6cfe58
Gergo Erdosi <gergo@timble.net>
2010-11-01 15:29:51 +0000

    Re #1: Code cleanup. Removed remote helper, no longer needed.

--- scripts/migrator/migrator.php
+++ scripts/migrator/migrator.php
@@ -3,27 +3,33 @@
 /** $Id$ */
 
 $config = array(
-	'host'		=> '',
-	'port'		=> 0,
-	'username'	=> '',
-	'password'	=> '',
-	'remote_path'	=> '/var/www/private'
+	'ssh'	=> array(
+		'host'		=> '',
+		'port'		=> 0,
+		'username'	=> '',
+		'password'	=> ''
+	),
+	'mysql'	=> array(
+		'username'	=> '',
+		'password'	=> ''
+	),
+	'document_root'	=> '/var/www'
 );
 
 // Validate the arguments.
-if($_SERVER["argc"] < 2 || substr($_SERVER["argv"][1], 0, 7) != '--site=')
+if($_SERVER['argc'] < 2 || substr($_SERVER['argv'][1], 0, 7) != '--site=')
 {
 	echo "Usage: migrator.php --site=1234\n\n";
 	exit;
 }
 
-$site = trim(substr($_SERVER["argv"][1], 7));
+$site = trim(substr($_SERVER['argv'][1], 7));
 
 // Connect to the server.
 echo 'Connecting...';
 
-if($connection = ssh2_connect($config['host'], $config['port'])) {
-	echo "\t\t\tSUCCESS\n";
+if($connection = ssh2_connect($config['ssh']['host'], $config['ssh']['port'])) {
+	echo "\t\t\tOK\n";
 }
 else
 {
@@ -34,8 +40,8 @@ else
 // Authenticate.
 echo 'Authenticating...';
 
-if(@ssh2_auth_password($connection, $config['username'], $config['password'])) {
-	echo "\t\tSUCCESS\n";
+if(@ssh2_auth_password($connection, $config['ssh']['username'], $config['ssh']['password'])) {
+	echo "\t\tOK\n";
 }
 else
 {
@@ -46,14 +52,13 @@ else
 // Validate the site.
 echo 'Validating site...';
 
-$stream = ssh2_exec($connection, $config['remote_path'].'/remote.php --action=validate --site='.$site);
+$stream		= ssh2_exec($connection, 'test -d '.$config['document_root'].'/'.$site.' && echo "OK" || echo "FAILED"');
 stream_set_blocking($stream, true);
-
-$response = fread($stream, 4096);
+$response	= fread($stream, 4096);
 fclose($stream);
 
-if($response == 'SUCCESS') {
-	echo "\t\tSUCCESS\n";
+if($response == 'OK') {
+	echo "\t\tOK\n";
 }
 else
 {
@@ -64,13 +69,23 @@ else
 // Compress the site.
 echo 'Compressing site...';
 
-$stream = ssh2_exec($connection, $config['remote_path'].'/remote.php --action=compress --site='.$site);
+$stream		= ssh2_exec($connection, 'mysqldump --user="'.$config['mysql']['username'].'" --password="'.$config['mysql']['password'].'" --add-drop-table *database* | gzip > '.$config['document_root'].'/'.$site.'/database.sql.gz');
 stream_set_blocking($stream, true);
-
-$response = fread($stream, 4096);
+$response	= fread($stream, 4096);
 fclose($stream);
 
-if($response == 'SUCCESS') {
+if($response != '')
+{
+	echo "\t\tFAILED\n\n";
+	exit;
+}
+
+$stream		= ssh2_exec($connection, 'tar -czf /tmp/'.md5($site).'.tar.gz '.$config['document_root'].'/'.$site.'/public');
+stream_set_blocking($stream, true);
+$response	= fread($stream, 4096);
+fclose($stream);
+
+if($response == '') {
 	echo "\t\tSUCCESS\n";
 }
 else
@@ -90,3 +105,5 @@ else
 	echo "\t\tFAILED\n\n";
 	exit;
 }
+
+echo "\n";
