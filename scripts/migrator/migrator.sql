428e6f64b2c107696a7d92a31affd5e59f5acc00
Gergo Erdosi <gergo@timble.net>
2010-11-03 16:09:15 +0000

    Re #1: Added admin renaming to migration SQL.

--- scripts/migrator/migrator.sql
+++ scripts/migrator/migrator.sql
@@ -2,3 +2,5 @@ DROP TABLE `pol_session`;
 
 CREATE VIEW `pol_session` AS
 	SELECT * FROM `police_default`.`pol_session`;
+
+UPDATE `pol_users` SET `username` = 'administrator' WHERE `username` = 'admin';
