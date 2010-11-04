f05511f4161a561a3c1dfe5e78136b6be9e9b392
Gergo Erdosi <gergo@timble.net>
2010-11-04 01:07:23 +0000

    Re #1: Changed script to rename site and database to four digit.

--- scripts/migrator/migrator.php
+++ scripts/migrator/migrator.php
@@ -30,13 +30,9 @@ if(!isset($arguments['site']))
 	exit;
 }
 
-$site		= $arguments['site'];
-$site_md5	= md5($site);
-
-// Set mysql username.
-if(empty($config['mysql']['username'])) {
-	$config['mysql']['username'] = 'tor'.$site;
-}
+$site_old	= $arguments['site'];
+$site_new	= substr($site_old, 0, 4);
+$site_old_md5	= md5($site_old);
 
 // Connect to the server.
 echo 'Connecting...';
@@ -65,7 +61,7 @@ else
 // Validate the site.
 echo 'Validating site...';
 
-$stream		= ssh2_exec($connection, 'test -d '.$config['document_root'].'/'.$site.' && echo "OK" || echo "FAILED"');
+$stream		= ssh2_exec($connection, 'test -d '.$config['document_root'].'/'.$site_old.' && echo "OK" || echo "FAILED"');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
 fclose($stream);
@@ -82,28 +78,28 @@ else
 // Compress the site.
 echo 'Compressing site...';
 
-$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site.'/configuration.php | grep \'var $db = \'');
+$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site_old.'/configuration.php | grep \'var $db = \'');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
 fclose($stream);
 
 $config['mysql']['database'] = trim(substr(trim($response), 10), ' \'";');
 
-$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site.'/configuration.php | grep \'var $user = \'');
+$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site_old.'/configuration.php | grep \'var $user = \'');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
 fclose($stream);
 
 $config['mysql']['username'] = trim(substr(trim($response), 12), ' \'";');
 
-$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site.'/configuration.php | grep \'var $password = \'');
+$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site_old.'/configuration.php | grep \'var $password = \'');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
 fclose($stream);
 
 $config['mysql']['password'] = trim(substr(trim($response), 16), ' \'";');
 
-$stream		= ssh2_exec($connection, 'mysqldump --user="'.$config['mysql']['username'].'" --password="'.$config['mysql']['password'].'" --add-drop-database --databases '.$config['mysql']['database'].' | gzip > '.$config['document_root'].'/'.$site.'/database.sql.gz');
+$stream		= ssh2_exec($connection, 'mysqldump --user="'.$config['mysql']['username'].'" --password="'.$config['mysql']['password'].'" --add-drop-database --databases '.$config['mysql']['database'].' | gzip > '.$config['document_root'].'/'.$site_old.'/database.sql.gz');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
 fclose($stream);
@@ -114,7 +110,7 @@ if($response != '')
 	exit;
 }
 
-$stream		= ssh2_exec($connection, 'tar -C '.$config['document_root'].'/'.$site.' -czf /tmp/'.$site_md5.'.tar.gz images dmdocuments database.sql.gz configuration.php templates/rhuk_milkyway_police/images/mw_joomla_logo.png');
+$stream		= ssh2_exec($connection, 'tar -C '.$config['document_root'].'/'.$site_old.' -czf /tmp/'.$site_old_md5.'.tar.gz images dmdocuments database.sql.gz configuration.php templates/rhuk_milkyway_police/images/mw_joomla_logo.png');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
 fclose($stream);
@@ -131,7 +127,7 @@ else
 // Retrieve the compressed site.
 echo 'Retrieving site...';
 
-if(ssh2_scp_recv($connection, '/tmp/'.$site_md5.'.tar.gz', '/tmp/'.$site_md5.'.tar.gz')) {
+if(ssh2_scp_recv($connection, '/tmp/'.$site_old_md5.'.tar.gz', '/tmp/'.$site_old_md5.'.tar.gz')) {
 	echo "\t\tOK\n";
 }
 else
@@ -143,38 +139,38 @@ else
 // Uncompress the site.
 echo 'Uncompressing site...';
 
-mkdir('/tmp/'.$site_md5, 0775);
-shell_exec('cd /tmp/'.$site_md5.' && tar -xzf ../'.$site_md5.'.tar.gz');
+mkdir('/tmp/'.$site_old_md5, 0775);
+shell_exec('cd /tmp/'.$site_old_md5.' && tar -xzf ../'.$site_old_md5.'.tar.gz');
 
 echo "\t\tOK\n";
 
 // Move files.
 echo 'Moving files...';
 
-if(!is_dir('/var/www/public/sites/'.$site)) {
-	mkdir('/var/www/public/sites/'.$site);
+if(!is_dir('/var/www/public/sites/'.$site_new)) {
+	mkdir('/var/www/public/sites/'.$site_new);
 }
 
-if(is_dir('/tmp/'.$site_md5.'/images'))
+if(is_dir('/tmp/'.$site_old_md5.'/images'))
 {
-	if(!is_dir('/var/www/public/sites/'.$site.'/images')) {
-		mkdir('/var/www/public/sites/'.$site.'/images');
+	if(!is_dir('/var/www/public/sites/'.$site_new.'/images')) {
+		mkdir('/var/www/public/sites/'.$site_new.'/images');
 	}
 
-	shell_exec('cp -R /tmp/'.$site_md5.'/images/* /var/www/public/sites/'.$site.'/images/');
+	shell_exec('cp -R /tmp/'.$site_old_md5.'/images/* /var/www/public/sites/'.$site_new.'/images/');
 }
 
-if(is_dir('/tmp/'.$site_md5.'/dmdocuments'))
+if(is_dir('/tmp/'.$site_old_md5.'/dmdocuments'))
 {
-	if(!is_dir('/var/www/public/sites/'.$site.'/documents')) {
-		mkdir('/var/www/public/sites/'.$site.'/documents');
+	if(!is_dir('/var/www/public/sites/'.$site_new.'/documents')) {
+		mkdir('/var/www/public/sites/'.$site_new.'/documents');
 	}
 
-	shell_exec('cp -R /tmp/'.$site_md5.'/dmdocuments/* /var/www/public/sites/'.$site.'/documents/');
+	shell_exec('cp -R /tmp/'.$site_old_md5.'/dmdocuments/* /var/www/public/sites/'.$site_new.'/documents/');
 }
 
-if(file_exists('/tmp/'.$site_md5.'/templates/rhuk_milkyway_police/images/mw_joomla_logo.png')) {
-	copy('/tmp/'.$site_md5.'/templates/rhuk_milkyway_police/images/mw_joomla_logo.png', '/var/www/public/sites/'.$site.'/logo.png');
+if(file_exists('/tmp/'.$site_old_md5.'/templates/rhuk_milkyway_police/images/mw_joomla_logo.png')) {
+	copy('/tmp/'.$site_old_md5.'/templates/rhuk_milkyway_police/images/mw_joomla_logo.png', '/var/www/public/sites/'.$site_new.'/logo.png');
 }
 
 echo "\t\t\tOK\n";
@@ -182,18 +178,18 @@ echo "\t\t\tOK\n";
 // Import database.
 echo 'Importing database...';
 
-shell_exec('cd /tmp/'.$site_md5.' && gunzip database.sql.gz');
-shell_exec('cd /tmp/'.$site_md5.' && mv database.sql database.sql.old');
-shell_exec('cd /tmp/'.$site_md5.' && sed -e \'s/http:\/\/217.21.184.146\/'.$site.'\///g\' -e \'s/`jos_/`pol_/g\' database.sql.old > database.sql');
-shell_exec('cd /tmp/'.$site_md5.' && mysql --user="root" --password="" < database.sql');
-shell_exec('mysql --user="root" --password="" police_'.$site.' < migrator.sql');
+shell_exec('cd /tmp/'.$site_old_md5.' && gunzip database.sql.gz');
+shell_exec('cd /tmp/'.$site_old_md5.' && mv database.sql database.sql.old');
+shell_exec('cd /tmp/'.$site_old_md5.' && sed -e \'s/http:\/\/217.21.184.146\/'.$site_old.'\///g\' -e \'s/`jos_/`pol_/g\' -e \'s/`'.$config['mysql']['database'].'`/`police_'.$site_new.'`/g\' database.sql.old > database.sql');
+shell_exec('cd /tmp/'.$site_old_md5.' && mysql --user="root" --password="" < database.sql');
+shell_exec('mysql --user="root" --password="" police_'.$site_new.' < migrator.sql');
 
 echo "\t\tOK\n";
 
 // Create configuration.
 echo 'Creating configuration...';
 
-if(file_exists($file = '/tmp/'.$site_md5.'/configuration.php'))
+if(file_exists($file = '/tmp/'.$site_old_md5.'/configuration.php'))
 {
 	foreach($lines = file($file) as $line)
 	{
@@ -216,8 +212,8 @@ if(file_exists($file = '/tmp/'.$site_md5.'/configuration.php'))
 
 	$content[] = '}';
 
-	file_put_contents('/var/www/public/sites/'.$site.'/configuration.php', implode(PHP_EOL, $content));
-	chmod('/var/www/public/sites/'.$site.'/configuration.php', 0444);
+	file_put_contents('/var/www/public/sites/'.$site_new.'/configuration.php', implode(PHP_EOL, $content));
+	chmod('/var/www/public/sites/'.$site_new.'/configuration.php', 0444);
 }
 
 echo "\tOK\n";
@@ -225,8 +221,8 @@ echo "\tOK\n";
 // Cleanup files.
 echo 'Cleaning up files...';
 
-shell_exec('rm -rf /tmp/'.$site_md5);
-shell_exec('rm /tmp/'.$site_md5.'.tar.gz');
+shell_exec('rm -rf /tmp/'.$site_old_md5);
+shell_exec('rm /tmp/'.$site_old_md5.'.tar.gz');
 
 echo "\t\tOK\n";
 
@@ -234,6 +230,6 @@ $users = array(
 	'gergo@timble.net'
 );
 
-mail(implode(', ', $users), 'Site migration completed', 'Migration of site \''.$site.'\' completed.');
+mail(implode(', ', $users), 'Site migration completed', 'Migration of site \''.$site_old.'\' completed.');
 
 echo "\n";
