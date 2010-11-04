d3a706cde309fbd35883b83986943d80e4804d60
Gergo Erdosi <gergo@timble.net>
2010-11-04 00:06:35 +0000

    Re #1: Added code to read database name from configuration.php (WIP).

--- scripts/migrator/migrator.php
+++ scripts/migrator/migrator.php
@@ -85,6 +85,14 @@ else
 // Compress the site.
 echo 'Compressing site...';
 
+$stream		= ssh2_exec($connection, 'cat '.$config['document_root'].'/'.$site.'configuration.php | grep \'var $db = \'');
+stream_set_blocking($stream, true);
+$response	= trim(fread($stream, 4096));
+fclose($stream);
+
+$database = trim(substr(trim($response, 10)), ' \'";');
+var_dump($database);exit;
+
 $stream		= ssh2_exec($connection, 'mysqldump --user="'.$config['mysql']['username'].'" --password="'.$config['mysql']['password'].'" --add-drop-database --databases police_'.$site.' | gzip > '.$config['document_root'].'/'.$site.'/database.sql.gz');
 stream_set_blocking($stream, true);
 $response	= trim(fread($stream, 4096));
