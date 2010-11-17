8dde77ad7da2b2f02a72682db998538676df6f0f
Gergo Erdosi <gergo@timble.net>
2010-11-17 17:04:33 +0000

    Re #6: Fixed path issue.

--- code/administrator/components/com_phocagallery/views/phocagalleryd/tmpl/default.php
+++ code/administrator/components/com_phocagallery/views/phocagalleryd/tmpl/default.php
@@ -2,17 +2,17 @@
 <table border="0" width="100%">
 	<tr>
 		<td align="center" valign="middle" height="486"><?php
-			if (isset($this->file->extid) && $this->file->extid !='') {				
+			if (isset($this->file->extid) && $this->file->extid !='') {
 				$resW	= explode(',', $this->file->extw);
 				$resH	= explode(',', $this->file->exth);
 
 				$correctImageRes = PhocaGalleryImage::correctSizeWithRate($resW[0], $resH[0], 640, 480);
 				echo '<a href="#" onclick="window.parent.document.getElementById(\'sbox-window\').close();">'. JHTML::_('image', $this->file->extl .'?imagesid='.md5(uniqid(time())), '').'</a>';
-		
+
 			} else if ($this->file->linkthumbnailpath=='') {
 				echo '<center style="font-size:large;font-weight:bold;color:#b3b3b3;font-family: Helvetica, sans-serif;">'. JText::_( 'Filename does not exist' ).'</center>';
 			} else {
-				echo '<a href="#" onclick="window.parent.document.getElementById(\'sbox-window\').close();">'. JHTML::_('image', $this->file->linkthumbnailpath .'?imagesid='.md5(uniqid(time())), '').'</a>';
+				echo '<a href="#" onclick="window.parent.document.getElementById(\'sbox-window\').close();">'. JHTML::_('image', JURI::root(false, '').substr($this->file->linkthumbnailpath, 1).'?imagesid='.md5(uniqid(time())), '').'</a>';
 			}
 			?>
 		</td>
