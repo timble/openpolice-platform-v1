982493815ed0b0ec118c3baf2b70d3b65f1fc8b7
Gergo Erdosi <gergo@timble.net>
2010-11-17 17:42:16 +0000

    Re #9: Added french language pack.

---
+++ code/plugins/editors/tinymce/jscripts/tiny_mce/plugins/advcode/langs/fr.js
@@ -0,0 +1,20 @@
+﻿// Le portail de présentation francophone de JCE: jce.sarki.ch 
+// Traductions et supports francophones par Sarki: www.sarki.ch
+tinyMCE.addI18n('fr.advcode',{
+desc : 'Insérer/Editer du Code',
+wrap:"Retour automatique à la ligne",
+highlight:"Coloration syntaxique",
+language:"Langue",
+numbers:"Numérotation des lignes",
+autocomplete:"fermeture des balises",
+'fontsize':"Taille de police",
+strong: 'Gras',
+em: 'Italique',
+underline: 'Souligné',
+removeformat: 'Supprimer le format',
+format: 'Format',
+undo: 'Annuler',
+redo: 'Rétablir',
+delta_width : 0,
+delta_height : 0
+});
---
+++ code/plugins/editors/tinymce/jscripts/tiny_mce/plugins/advcode/langs/fr_dlg.js
@@ -0,0 +1,18 @@
+﻿// Le portail de présentation francophone de JCE: jce.sarki.ch 
+// Traductions et supports francophones par Sarki: www.sarki.ch
+tinyMCE.addI18n('fr.advcode_dlg',{
+title:"Editeur de Code Etendu",
+wordwrap:"Retour automatique",
+highlight:"Coloration syntaxique",
+language:"Code :",
+numbers:"Numérotation des lignes",
+autocomplete:"Fermeture des guillemets",
+'fontsize':"Taille",
+strong: 'Gras',
+em: 'Italique',
+underline: 'Souligné',
+removeformat: 'Supprimer le(s) style(s)',
+format: 'Format',
+undo: 'Annuler',
+redo: 'Rétablir'
+});
