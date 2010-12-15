d38ee7e1ad03907c58b1029dd90c59c2fd1b6f11
Johan Janssens <johan@timble.net>
2010-12-15 19:22:13 +0000

    Removed online admin user count.

--- code/administrator/modules/mod_online/mod_online.php
+++ code/administrator/modules/mod_online/mod_online.php
@@ -13,18 +13,3 @@
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
-
-$db			=& JFactory::getDBO();
-$session		=& JFactory::getSession();
-
-$session_id = $session->getId();
-
-// Get no. of users online not including current session
-$query = 'SELECT COUNT( session_id )'
-. ' FROM #__session'
-. ' WHERE session_id <> '.$db->Quote($session_id)
-;
-$db->setQuery($query);
-$online_num = intval( $db->loadResult() );
-
-echo $online_num . ' <img src="images/users.png" align="middle" alt="'. JText::_( 'Users Online' ) .'" />';
