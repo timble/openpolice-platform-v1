0cd43b0855b3bb2b32ea882e1b27b8e476808045
Gergo Erdosi <gergo@timble.net>
2010-12-19 23:47:50 +0000

    Updated DOCman configuration file.

--- code/administrator/components/com_docman/docman.config.php
+++ code/administrator/components/com_docman/docman.config.php
@@ -1,15 +1,15 @@
 <?php
 if(defined('_dmConfig')) {
 return true;
-} else { 
-define('_dmConfig',1); 
+} else {
+define('_dmConfig',1);
 
 class dmConfig
 {
 var $author_can = '2';
 var $days_for_new = '5';
-var $default_editor = '-6';
-var $default_order = 'name';
+var $default_editor = '-11';
+var $default_order = 'date';
 var $default_order2 = 'DESC';
 var $default_reader = '0';
 var $default_viewer = '-1';
@@ -23,10 +23,10 @@ var $fname_blank = '0';
 var $fname_lc = '0';
 var $fname_reject = 'index.htm|index.html|index.php';
 var $hide_remote = '1';
-var $hot = '100';
+var $hot = '20';
 var $icon_size = '1';
 var $icon_theme = 'default';
-var $individual_perm = '1';
+var $individual_perm = '0';
 var $isDown = '0';
 var $log = '0';
 var $maintainer = '1';
@@ -37,8 +37,8 @@ var $methods = array (
   2 => 'transfer',
 );
 var $overwrite = '0';
-var $perpage = '5';
-var $process_bots = '0';
+var $perpage = '25';
+var $process_bots = '1';
 var $reader_assign = '3';
 var $registered = '2';
 var $security_allowed_hosts = 'localhost';
@@ -46,9 +46,9 @@ var $security_anti_leech = '0';
 var $specialcompat = '0';
 var $trimwhitespace = '1';
 var $user_all = '0';
-var $user_approve = '-3';
-var $user_publish = '-3';
-var $user_upload = '0';
+var $user_approve = '-11';
+var $user_publish = '-11';
+var $user_upload = '-11';
 var $viewtypes = 'html|htm|txt|jpg|jpeg|gif|png';
 }
 }
