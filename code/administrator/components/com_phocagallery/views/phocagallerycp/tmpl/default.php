305579da2c060302765e6f5a4836e3ca91a0e399
Johan Janssens <johan@timble.net>
2011-01-12 01:35:12 +0000

    Cleaned up Phocagallery dashboard

--- code/administrator/components/com_phocagallery/views/phocagallerycp/tmpl/default.php
+++ code/administrator/components/com_phocagallery/views/phocagallerycp/tmpl/default.php
@@ -35,17 +35,6 @@
 	$link = 'index.php?option=com_phocagallery&view=phocagalleryin';
 	echo PhocaGalleryRenderAdmin::quickIconButton( $link, 'icon-48-pg-info.png', JText::_( 'Info' ) );
 	?>
-			
-			<div style="clear:both">&nbsp;</div>
-			<p>&nbsp;</p>
-			<div style="text-align:center;padding:0;margin:0;border:0">
-				<iframe style="padding:0;margin:0;border:0" src="http://www.phoca.cz/adv/phocagallery" noresize="noresize" frameborder="0" border="0" cellspacing="0" scrolling="no" width="500" marginwidth="0" marginheight="0" height="125">
-				<a href="http://www.phoca.cz/adv/phocagallery" target="_blank">Phoca Gallery</a>
-				</iframe> 
-			</div>
-			
-			
-			</div>
 		</td>
 		
 		<td width="45%" valign="top">
@@ -63,26 +52,6 @@
 
 			<h3><?php echo JText::_('License');?></h3>
 			<p><a href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GPLv2</a></p>
-			<p>&nbsp;</p>
-			
-			<p><strong><?php echo JText::_('SHADOW BOX LICENSE HEAD');?></strong></p>
-			<p class="license"><?php echo JText::_('SHADOW BOX LICENSE');?></p>
-			<p><a href="http://mjijackson.com/shadowbox/" target="_blank">Shadowbox.js</a> by <a target="_blank" href="http://www.mjijackson.com">Michael J. I. Jackson</a><br />
-			<a target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-Noncommercial-Share Alike</a></p>
-			
-
-			
-			<p><strong><?php echo JText::_('HIGHSLIDE LICENSE HEAD');?></strong></p>
-			<p class="license"><?php echo JText::_('HIGHSLIDE LICENSE');?></p>
-			<p><a href="http://highslide.com/" target="_blank">Highslide JS</a> by <a target="_blank" href="http://highslide.com/">Torstein Hønsi</a><br />
-			<a target="_blank" href="http://creativecommons.org/licenses/by-nc/2.5/">Creative Commons Attribution-NonCommercial 2.5  License</a></p>
-			
-			<p>Google™, Google Maps™, Google Picasa™ and YouTube Broadcast Yourself™ are registered trademarks of Google Inc.</p>
-			
-			<div id="pg-update"><a href="http://www.phoca.cz/version/index.php?phocagallery=<?php echo $this->version ;?>" target="_blank"><?php echo JText::_('Check for update'); ?></a></div>
-			
-			
-			</div>
 		</td>
 	</tr>
 </table>
