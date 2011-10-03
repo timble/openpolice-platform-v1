2caad0f6b4e23556c968df581d1e08541369cb66
Gergo Erdosi <gergo@timble.net>
2011-10-03 11:19:08 +0000

    Merge releases/staging to releases/production.

--- code/templates/default-v0/index.php
+++ code/templates/default-v0/index.php
@@ -3,7 +3,7 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 include_once("includes/grid.php");
 
-$version = '5';
+$version = '6';
 $language = explode("-", $this->language);
 $language = $language[0];
 
