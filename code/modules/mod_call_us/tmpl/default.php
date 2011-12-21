31c38ff436463725ace6df755fddb7e779b1e686
Gergo Erdosi <gergo@timble.net>
2011-12-21 01:53:05 +0000

    Merge releases/staging to releases/production.

--- code/modules/mod_call_us/tmpl/default.php
+++ code/modules/mod_call_us/tmpl/default.php
@@ -1,4 +1,4 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); ?>
 
-<strong><?php echo JText::_('Call Us'); ?>: <?php echo $params->get('phone'); ?></strong><?php if ($params->get('emergency') == '1') : ?> - <?php echo JText::_('In an emergency'); ?>: 112<?php endif ?>
+<strong><?php echo JText::_('Call Us'); ?>: <?php echo $params->get('phone'); ?></strong><?php if ($params->get('emergency') == '1') : ?> - <?php echo JText::_('In an emergency'); ?>: 101<?php endif ?>
