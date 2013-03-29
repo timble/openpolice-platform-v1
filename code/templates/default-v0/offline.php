2a68d3e67e3c8e5cc84ebbe39efbd1fb10306784
Tom Janssens <tom@timble.net>
2013-03-29 15:02:03 +0100

    New banner for site 5323 re #343

--- code/templates/default-v0/offline.php
+++ code/templates/default-v0/offline.php
@@ -2,7 +2,7 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-$version = '34';
+$version = '35';
 $language = explode("-", $this->language);
 $language = $language[0];
 
