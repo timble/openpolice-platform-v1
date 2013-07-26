43f3081f435da2137ca6bf24a2d96e4dad1a3fc6
Steven Rombauts <steven@timble.net>
2013-07-26 12:50:34 +0100

    re #512 - Add terminal scripts + README

---
+++ scripts/terminal/rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script will attempt to connect to the police-production server
+# and rsync the mysql dumps and www files
+#
+# Note: this script requires rsync 3.x with iconv support.
+# Get it from, for example, using homebrew: brew install https://raw.github.com/Homebrew/homebrew-dupes/master/rsync.rb
+
+/usr/local/bin/rsync -avz --delete -e "ssh -p 9999" deploy@127.0.10.10:/var/backups/databases/ /Users/terminal/Sites/lokalepolitie.be/backups/databases
+/usr/local/bin/rsync -avz --delete --iconv=UTF8-MAC,UTF8 -e "ssh -p 9999" --no-links --exclude-from 'rsync.exclude' deploy@127.0.10.10:/var/www/lokalepolitie.be/ /Users/terminal/Sites/lokalepolitie.be/backups/files/
