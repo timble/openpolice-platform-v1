30f6d1aae28b769e7cc71dd55313560903e407f9
Steven Rombauts <steven@timble.net>
2013-07-27 09:33:43 +0100

    re #509 - Finished nucleus sync script

--- scripts/terminal/nucleus-sync.sh
+++ scripts/terminal/nucleus-sync.sh
@@ -9,7 +9,10 @@ echo "-- Dump all databases on Nucleus production (please supply fedpol password
 ssh -t -p 9999 fedpol@188.93.153.147 << EOF
   mkdir /tmp/dumps
   mysql -N -ufedpol -pVNW6eatrdtyknQ -e 'SHOW DATABASES' | while read dbname; do mysqldump -ufedpol -pVNW6eatrdtyknQ --complete-insert --add-drop-table --extended-insert --quote-names \$dbname > /tmp/dumps/\$dbname.sql; done;
-  tar -cvzf /tmp/dumps/production-backup.tar.gz /tmp/dumps/*.sql
+
+  cd /tmp/dumps
+  tar -cvzf /tmp/dumps/production-backup.tar.gz *.sql
+  cd /tmp
   rm -f /tmp/dumps/*.sql
 EOF
 
@@ -22,26 +25,23 @@ ssh -t -p 9999 fedpol@188.93.153.147 "rm -rf /tmp/dumps"
 echo "-- Upload the dumps to SS production .."
 scp -P 9999 /tmp/production-backup.tar.gz deploy@127.0.10.10:/tmp/production-backup.tar.gz
 
-echo "-- Import all databases on SS server .. "
+echo "-- Import all databases on SS server .. (supply fedpol password)"
 ssh -t -p 9999 fedpol@127.0.10.10 << EOF
   mkdir /tmp/dumps
   cd /tmp/dumps
   tar -xvzf /tmp/production-backup.tar.gz -C /tmp/dumps
 
-  for $dump in *.sql; do
-    mysql -N -ufedpol -pVNW6eatrdtyknQ ${dump%.*} < $dump
+  for dump in *.sql
+  do
+    mysql -N -ufedpol -pVNW6eatrdtyknQ "\${dump%.*}" < "\$dump"
   done
 
   rm -rf /tmp/dumps
   rm -f /tmp/production-backup.tar.gz
 EOF
 
-
-exit;
-
-
 echo "-- Making a copy of the local backup files .."
-# cp -aR /Users/terminal/Sites/lokalepolitie.be/backups/files/capistrano/shared/sites /tmp/
+cp -aR /Users/terminal/Sites/lokalepolitie.be/backups/files/capistrano/shared/sites /tmp/
 
 echo "-- Syncing with Nucleus (please supply fedpol password) .."
 /usr/local/bin/rsync --progress --iconv=UTF8-MAC,UTF8 --rsh "ssh -p 9999" fedpol@188.93.153.147:/var/www/lokalepolitie.be/capistrano/shared/sites/ /tmp/sites/ --update --perms --owner --group --recursive --times --no-links
