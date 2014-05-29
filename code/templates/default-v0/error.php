886ff78e1965b84509d12fc7a5e516a1e28e0445
Tom Janssens <tom@timble.net>
2014-05-29 13:40:46 +0200

    re #721 - Tracking was not working cross domain

--- code/templates/default-v0/error.php
+++ code/templates/default-v0/error.php
@@ -38,7 +38,7 @@ $site = JFactory::getApplication()->getSite();
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-20242887-1', 'lokalepolitie.be');
+        ga('create', 'UA-20242887-1', 'auto');
         ga('send', 'pageview');
     </script>
 	<?php endif; ?>
